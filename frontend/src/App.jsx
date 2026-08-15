402d8f103475c13108a822f65c5ddcdc3e49dc71
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-08-15 21:27:03 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
+import { Route,Routes,BrowserRouter } from "react-router-dom"
+import Landing from "./landing/landing.jsx"
+import Register from "./register/register.jsx"
+import Login from "./login/login.jsx"
+
 function App(){
   return <>
-  
+  <Routes>
+<Route path="/" element={<Landing />}></Route>
+<Route path="/Login" element={<Login />} />
+<Route path="/Register" element={<Register />} />
+
+
+
+  </Routes>
   </>
 }
 export default App

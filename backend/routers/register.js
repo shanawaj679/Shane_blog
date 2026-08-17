25ad4e105618fe3b4dd2baea1ac56ada16077c0f
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-08-17 22:13:22 +0530

    register route is created and updated

---
+++ backend/routers/register.js
@@ -0,0 +1,7 @@
+import express from "express"
+import db from "../config/db.js"
+import register from "../controller/register.js"
+
+const router = express.Router()
+router.post("/",register)
+export default router

542b43613cffa7c1782670d1d1951f8e289df6c0
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-08-18 21:33:33 +0530

    added rate limitor for login

--- backend/routers/login.js
+++ backend/routers/login.js
@@ -1,6 +1,12 @@
 import express from "express"
 import db from "../config/db.js"
+import rateLimit from "express-rate-limit"
 import login from "../controller/login.js"
 const router = express.Router()
-router.post("/",login)
+const login_limit = rateLimit({
+    windowMs:5*60*1000,
+    max:3,
+    message:{message:"too many login request please try again after 5 min."}
+})
+router.post("/",login_limit,login)
 export default router

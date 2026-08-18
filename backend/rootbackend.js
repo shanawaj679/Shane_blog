0607759b4c4467b07f7d57762d69ca5d8b34cee8
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-08-18 08:17:05 +0530

    login routes are added

--- backend/rootbackend.js
+++ backend/rootbackend.js
@@ -7,10 +7,12 @@ app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(express.json())
 
 import register from "./routers/register.js"
+import login from "./routers/login.js"
 
 
 
 app.use("/api/register",register);
+app.use("/api/login",login)
 
 
 app.listen(3000,()=>{
